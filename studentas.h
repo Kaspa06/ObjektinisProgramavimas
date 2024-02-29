1d351a2bb5bae524691893537becbce926171bb7
Kaspa06 <159523672+Kaspa06@users.noreply.github.com>
2024-02-29 20:12:17 +0200

    Update studentas.h

--- studentas.h
+++ studentas.h
@@ -1,1 +1,25 @@
+#ifndef STUDENTAS_H
+#define STUDENTAS_H
 
+#include <string>
+#include <vector>
+
+struct Student {
+    std::string Vardas;
+    std::string Pavarde;
+    int n;
+    std::vector<double> nd;
+    double egz;
+    double galutinis;
+    double galutinisMed;
+    double vidurkis;
+};
+
+std::vector<Student> Studentai;
+
+void inputStudentInfo(Student& student);
+void inputGrades(Student& student);
+void inputExamResult(Student& student);
+void calculateResults(Student& student, int calculationMethod);
+
+#endif
