607d0cbf3fde20a994d0c385f3dfd283894bba67
Kaspa06 <159523672+Kaspa06@users.noreply.github.com>
2024-02-29 20:12:43 +0200

    Update funkcijos.h

--- funkcijos.h
+++ funkcijos.h
@@ -1,1 +1,16 @@
+#ifndef FUNKCIJOS_H
+#define FUNKCIJOS_H
 
+#include "studentas.h"
+#include <vector>
+#include <string>
+
+int mokiniuSk;
+
+void writeEverythingWithHands();
+void generateRandomGradeInput();
+void generateRandomStudentData(int mokiniuSk, int sum);
+void readDataFromFile();
+
+
+#endif
