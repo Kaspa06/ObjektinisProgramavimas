14a8f4ea63bfa28a3756929e743dce3016aea6c8
Kaspa06 <159523672+Kaspa06@users.noreply.github.com>
2024-03-07 19:53:51 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,13 @@
+#include "studentas.h"
+#include "funkcijos.h"
 #include <iostream>
 #include <vector>
-#include <string>
-#include <algorithm>
-#include <limits>
 #include <cstdlib>
 #include <ctime>
-#include <iomanip>
-#include <fstream>
-#include <sstream>
+#include <limits>
+#include <stdexcept>
 #include <chrono>
+#include <fstream>
 
 using namespace std;
 using namespace chrono;
@@ -22,17 +21,21 @@ int main() {
     int testuSkaicius = 0;
 
     vector<double> testuLaikai;
+    vector<int> sizes = {1000, 10000, 100000, 1000000, 10000000};
+    vector<Studentas> kietiakiai, vargsiukai;
 
-    while (pasirinkimas != 5) {
-        auto start = chrono::high_resolution_clock::now();
+    while (pasirinkimas != 7) {
+        auto startOperation = chrono::high_resolution_clock::now();
 
         cout << "\nMenu:\n";
         cout << "1. Ivesti viska rankomis\n";
         cout << "2. Generuoti pazymius\n";
         cout << "3. Generuoti ir pazymius ir studentu vardus, pavardes\n";
         cout << "4. Gauti studentu vardus, pavardes, pazymius is failo.\n";
-        cout << "5. Baigti darba\n";
-        cout << "Rinktis (1-5): ";
+        cout << "5. Generuoti studentu failus.\n";
+        cout << "6. Rusiuoti studentus sugeneruotuose failuose.\n";
+        cout << "7. Baigti darba\n";
+        cout << "Rinktis (1-7): ";
 
         cin >> pasirinkimas;
         if (!cin.good()) {
@@ -98,15 +101,54 @@ int main() {
                     break;
                 }
             case 5:
+                {
+                    auto startFileGeneration = std::chrono::high_resolution_clock::now();
+                    generateStudentFiles(sizes);
+                    auto endFileGeneration = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> elapsedFileGeneration = endFileGeneration - startFileGeneration;
+                    std::cout << "Failu generavimas uztruko: " << elapsedFileGeneration.count() << " sekundziu." << std::endl;
+                    break;
+                }
+            case 6:
+                {
+                    std::cout << "Pasirinkite, kuri studentu faila norite rusiuoti:" << std::endl;
+                    for (size_t i = 0; i < sizes.size(); ++i)
+                    {
+                        std::cout << i + 1 << " - studentai" << sizes[i] << ".txt" << std::endl;
+                    }
+                    std::cout << "Pasirinkimas: ";
+                    size_t choice;
+                    std::cin >> choice;
+
+                    if (std::cin.fail() || choice < 1 || choice > sizes.size())
+                    {
+                        std::cerr << "Neteisingas pasirinkimas." << std::endl;
+                        std::cin.clear();
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                        break;
+                    }
+
+                    auto startSorting = std::chrono::high_resolution_clock::now();
+                    rusiuotiStudentus(std::vector<int>{sizes[choice - 1]});
+                    auto endSorting = std::chrono::high_resolution_clock::now();
+                    std::chrono::duration<double> elapsedSorting = endSorting - startSorting;
+                    std::cout << "Rusiavimas ir issaugojimas uztruko: " << elapsedSorting.count() << " sekundziu." << std::endl;
+
+                    break;
+                }
+            case 7:
+                break;
+            default:
+                cout << "Neatpazintas pasirinkimas. Bandykite dar karta." << std::endl;
                 break;
         }
 
-        if (pasirinkimas != 5) {
-            auto end = chrono::high_resolution_clock::now();
-            chrono::duration<double> time = end - start;
-            double laikas = time.count();
-            cout << "Operacija uztruko: " << laikas << " s" << endl;
-            visoLaikoSuma += laikas;
+        if (pasirinkimas != 7) {
+            auto endOperation = chrono::high_resolution_clock::now();
+            chrono::duration<double> timeOperation = endOperation - startOperation;
+            double laikasOperation = timeOperation.count();
+            cout << "Operacija uztruko: " << laikasOperation << " s" << endl;
+            visoLaikoSuma += laikasOperation;
             testuSkaicius++;
         }
     }
