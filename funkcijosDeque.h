13d1e8f611053ce833f6cd9b38f965c0bbda6aca
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-03-25 20:44:25 +0200

    Update funkcijosDeque.h

--- funkcijosDeque.h
+++ funkcijosDeque.h
@@ -1,1 +1,11 @@
+#ifndef FUNKCIJOSDEQUE_H
+#define FUNKCIJOSDEQUE_H
 
+#include "studentas.h"
+#include <deque>
+
+void rusiuotiDeque1(const std::deque<int>& sizes);
+void rusiuotiDeque2(const std::deque<int>& sizes);
+void rusiuotiDeque3(const std::deque<int>& sizes);
+
+#endif
