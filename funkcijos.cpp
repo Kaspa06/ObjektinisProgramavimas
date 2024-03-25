4629789cd919f129427a865781bc81f383e1b75d
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-03-25 15:23:01 +0200

    Update funkcijos.cpp

--- funkcijos.cpp
+++ funkcijos.cpp
@@ -253,8 +253,6 @@ void rusiuotiStudentus(const std::vector<int>& sizes) {
         std::chrono::duration<double> elapsedRead = endRead - startRead;
         std::cout << "Duomenu nuskaitymas is " << fileName << " uztruko: " << elapsedRead.count() << " sekundziu." << std::endl;
 
-       // auto startSort = std::chrono::high_resolution_clock::now();
-
         for (const auto& studentas : studentai) {
             double galutinisBalas = 0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas;
             if (galutinisBalas < 5.0) {
@@ -263,12 +261,6 @@ void rusiuotiStudentus(const std::vector<int>& sizes) {
                 kietiakiai.push_back(studentas);
             }
         }
-
-        //auto endSort = std::chrono::high_resolution_clock::now();
-        //std::chrono::duration<double> elapsedSort = endSort - startSort;
-        //std::cout << "Studentu rusiavimas uztruko: " << elapsedSort.count() << " sekundziu." << std::endl;
-
-        // Isvedimas i failus
         std::ofstream kietiakiaiFile("kietiakiai.txt"), vargsiukaiFile("vargsiukai.txt");
 
         for (const auto &studentas : kietiakiai) {
@@ -281,5 +273,144 @@ void rusiuotiStudentus(const std::vector<int>& sizes) {
 
         kietiakiaiFile.close();
         vargsiukaiFile.close();
+    }
+}
+
+void rusiuotiStudentus2(const std::vector<int>& sizes) {
+    for (size_t index = 0; index < sizes.size(); ++index) {
+        std::string fileName = "studentai" + std::to_string(sizes[index]) + ".txt";
+        std::ifstream inFile(fileName);
+
+        if (!inFile) {
+            std::cerr << "Nepavyko atidaryti failo: " << fileName << std::endl;
+            continue;
+        }
+
+        std::vector<Studentas> studentai, vargsiukai;
+        Studentas tempStudentas;
+        std::string eilute;
+        std::getline(inFile, eilute); 
+
+        auto startRead = std::chrono::high_resolution_clock::now();
+
+        while (std::getline(inFile, eilute)) {
+            std::istringstream eiluteStream(eilute);
+            eiluteStream >> tempStudentas.vardas >> tempStudentas.pavarde;
+            tempStudentas.nd.clear();
+            int pazymys;
+
+            while (eiluteStream >> pazymys) {
+                tempStudentas.nd.push_back(pazymys);
+            }
+            
+            if (!tempStudentas.nd.empty()) {
+                tempStudentas.egzaminas = tempStudentas.nd.back();
+                tempStudentas.nd.pop_back();
+            }
+
+            studentai.push_back(tempStudentas);
+        }
+
+        inFile.close();
+
+        auto endRead = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsedRead = endRead - startRead;
+        std::cout << "Duomenu nuskaitymas is " << fileName << " uztruko: " << elapsedRead.count() << " sekundziu." << std::endl;
+
+        for (auto it = studentai.begin(); it != studentai.end(); ) {
+            double galutinisBalas = 0.4 * vidurkis(it->nd) + 0.6 * it->egzaminas;
+            if (galutinisBalas < 5.0) {
+                vargsiukai.push_back(*it);
+                it = studentai.erase(it);
+            } else {
+                ++it;
+            }
+        }
+
+        std::ofstream vargsiukaiFile("vargsiukai.txt");
+
+        for (const auto &studentas : vargsiukai) {
+            vargsiukaiFile << studentas.vardas << " " << studentas.pavarde << " " << std::fixed << std::setprecision(2) << (0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas) << std::endl;
+        }
+
+        vargsiukaiFile.close();
+
+        std::ofstream outFile(fileName);
+
+        for (const auto &studentas : studentai) {
+            outFile << studentas.vardas << " " << studentas.pavarde << " " << std::fixed << std::setprecision(2) << (0.4 * vidurkis(studentas.nd) + 0.6 * studentas.egzaminas) << std::endl;
+        }
+
+        outFile.close();
+    }
+}
+
+void rusiuotiStudentus3(const std::vector<int>& sizes) {
+    for (size_t index = 0; index < sizes.size(); ++index) {
+        std::string fileName = "studentai" + std::to_string(sizes[index]) + ".txt";
+        std::ifstream inFile(fileName);
+
+        if (!inFile) {
+            std::cerr << "Nepavyko atidaryti failo: " << fileName << std::endl;
+            continue;
+        }
+
+        std::vector<Studentas> studentai;
+        Studentas tempStudentas;
+        std::string eilute;
+        std::getline(inFile, eilute); 
+
+        auto startRead = std::chrono::high_resolution_clock::now();
+
+        while (std::getline(inFile, eilute)) {
+            std::istringstream eiluteStream(eilute);
+            eiluteStream >> tempStudentas.vardas >> tempStudentas.pavarde;
+            tempStudentas.nd.clear();
+            int pazymys;
+
+            while (eiluteStream >> pazymys) {
+                tempStudentas.nd.push_back(pazymys);
+            }
+            
+            if (!tempStudentas.nd.empty()) {
+                tempStudentas.egzaminas = tempStudentas.nd.back();
+                tempStudentas.nd.pop_back();
+            }
+
+            studentai.push_back(tempStudentas);
+        }
+
+        inFile.close();
+
+        auto endRead = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> elapsedRead = endRead - startRead;
+        std::cout << "Duomenu nuskaitymas is " << fileName << " uztruko: " << elapsedRead.count() << " sekundziu." << std::endl;
+
+        std::vector<Studentas> vargsiukai;
+
+        std::copy_if(studentai.begin(), studentai.end(), std::back_inserter(vargsiukai), [](const Studentas& s) {
+            double galutinisBalas = 0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas;
+            return galutinisBalas < 5.0;
+        });
+
+        studentai.erase(std::remove_if(studentai.begin(), studentai.end(), [](const Studentas& s) {
+            double galutinisBalas = 0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas;
+            return galutinisBalas < 5.0;
+        }), studentai.end());
+
+        std::ofstream vargsiukaiFile("vargsiukai.txt");
+        std::ofstream outFile(fileName);
+
+        std::for_each(vargsiukai.begin(), vargsiukai.end(), [&vargsiukaiFile](const Studentas& s) {
+            vargsiukaiFile << s.vardas << " " << s.pavarde << " " << std::fixed << std::setprecision(2) << (0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas) << std::endl;
+        });
+
+        // Write remaining students to outFile
+        std::for_each(studentai.begin(), studentai.end(), [&outFile](const Studentas& s) {
+            outFile << s.vardas << " " << s.pavarde << " " << std::fixed << std::setprecision(2) << (0.4 * vidurkis(s.nd) + 0.6 * s.egzaminas) << std::endl;
+        });
+
+        vargsiukaiFile.close();
+        outFile.close();
     }
 }
