bfcfb4ff36162e710e133f46225eb7efe4a27f2b
Kasparas <159523672+Kaspa06@users.noreply.github.com>
2024-03-25 20:44:47 +0200

    Update funkcijosList.h

--- funkcijosList.h
+++ funkcijosList.h
@@ -1,1 +1,11 @@
+#ifndef FUNKCIJOSLIST_H
+#define FUNKCIJOSLIST_H
 
+#include "studentas.h"
+#include <list>
+
+void rusiuotiList1(const std::list<int>& sizes);
+void rusiuotiList2(const std::list<int>& sizes);
+void rusiuotiList3(const std::list<int>& sizes);
+
+#endif
